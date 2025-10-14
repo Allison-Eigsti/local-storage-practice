a886494292211f993fe3dbb4a436bc173f876b7b
AllisonE-code <allisonmeigsti@gmail.com>
2025-10-14 18:20:59 +0200

    save to local storage functionality

---
+++ index.js
@@ -0,0 +1,54 @@
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
+const color4 = document.getElementById('color4');
+
+const colorPicker1 = document.getElementById('colorPicker1');
+const colorPicker2 = document.getElementById('colorPicker2');
+const colorPicker3 = document.getElementById('colorPicker3');
+const colorPicker4 = document.getElementById('colorPicker4');
+
+const singlePalette = {
+    title: '',
+    colors: ['', '', '', '']
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 1; i <= 4; i++) {
+        const color = document.getElementById(`color${i}`);
+        const picker = document.getElementById(`colorPicker${i}`);
+
+        if (color && picker) {
+            color.addEventListener('click', () => {
+                picker.click();
+            })
+
+            picker.addEventListener('input', (event) => {
+                const colorValue = event.target.value;
+                color.style.backgroundColor = colorValue;
+                singlePalette.colors[i - 1] = colorValue;
+            })
+        }
+    }
+})
+
+
+function saveToLocalStorage() {
+    const title = document.getElementById('palette-title').value || 'Untitled Palette';
+    singlePalette.title = `${title}`;
+    
+    let newPalette = {...singlePalette, colors: [...singlePalette.colors]};
+
+    let loadExistingPalettes = JSON.parse(localStorage.getItem('allPalettes') || [])
+    loadExistingPalettes.push(newPalette);
+
+    localStorage.setItem('allPalettes', JSON.stringify(loadExistingPalettes));
+    
+    window.location.href= 'library.html';
+};
+
+
+function load() {
+ console.log('hi');
+}
